5ea243a92d6bbe991106c86423610124ae9a97f7
YAMASAKI Masahide <masahide.y@gmail.com>
2024-04-07 12:30:37 +0900

    fix

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/masahide/mackerel-7dtd
+
+go 1.21.5
+
+require github.com/kelseyhightower/envconfig v1.4.0
+
+require github.com/mackerelio/mackerel-client-go v0.31.0 // indirect
---
+++ main.go
@@ -0,0 +1,379 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/mackerelio/mackerel-client-go"
+)
+
+const (
+	stateDirName  = "sdtd-monitor"
+	stateFileName = "sdtd-monitor"
+)
+
+type env struct {
+	Debug            bool   `envconfig:"DEBUG" default:"false"`
+	MackerelHostID   string `envconfig:"MACKEREL_HOST_ID"`
+	MackerelAPIKey   string `envconfig:"MACKEREL_API_KEY"`
+	PlayersAPIURL    string `envconfig:"PLAYERS_API_URL" default:""`
+	PlayersAPISecret string `envconfig:"PLAYERS_API_SECRET" default:""`
+	PlayersAPIUser   string `envconfig:"PLAYERS_API_USER" default:""`
+}
+
+type MetricDetail struct {
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName"`
+	IsStacked   bool   `json:"isStacked"` // 対応するメトリックを積み上げ表示するかどうか。偽なら折れ線で表示されます。
+}
+
+type MetricDef struct {
+	Name        string         `json:"name"` // メトリック名の最後の . より前の部分。 custom. ではじまる必要があります。また、ワイルドカード#, * を使用することもできます。
+	DisplayName string         `json:"displayName"`
+	Unit        string         `json:"unit"` // "float", "integer", "percentage", "seconds", "milliseconds", "bytes", "bytes/sec", "bits/sec", "iops"
+	Metrics     []MetricDetail `json:"metrics"`
+}
+
+type MetricValue struct {
+	HostID string  `json:"hostId"`
+	Name   string  `json:"name"`
+	Time   int64   `json:"time"`
+	Value  float64 `json:"value"`
+}
+
+// Player 構造体を更新して、新しいフィールドを含む
+type Player struct {
+	SteamID         string   `json:"steamid"`
+	CrossplatformID string   `json:"crossplatformid"`
+	EntityID        int      `json:"entityid"`
+	IP              string   `json:"ip"`
+	Name            string   `json:"name"`
+	Online          bool     `json:"online"`
+	Position        Position `json:"position"`
+	Level           float64  `json:"level"`
+	Health          float64  `json:"health"`
+	Stamina         float64  `json:"stamina"`
+	ZombieKills     float64  `json:"zombiekills"`
+	PlayerKills     float64  `json:"playerkills"`
+	PlayerDeaths    float64  `json:"playerdeaths"`
+	Score           float64  `json:"score"`
+	TotalPlayTime   int      `json:"totalplaytime"`
+	LastOnline      string   `json:"lastonline"`
+	Ping            int      `json:"ping"`
+}
+
+type Position struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+	Z float64 `json:"z"`
+}
+
+type mackerelAPI struct {
+	env
+	mkr       *mackerel.Client
+	steamIDs  []string
+	stateFile string
+}
+
+func jsonDump(v any) string {
+	b, _ := json.Marshal(v)
+	return string(b)
+}
+
+func reqDump(req *http.Request) string {
+	reqDump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		log.Printf("Error dumping request.  err:%s", err)
+		return ""
+	}
+	return string(reqDump)
+}
+func respDump(resp *http.Response) string {
+	respDump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Printf("Error dumping response.  err:%s", err)
+		return ""
+	}
+	return string(respDump)
+}
+
+func (m *mackerelAPI) getPlayersOnline() []Player {
+	res := []Player{}
+	req, err := http.NewRequest(http.MethodGet, m.PlayersAPIURL, nil)
+	if err != nil {
+		log.Fatalf("Error creating request.  err:%s", err)
+		return res
+	}
+	// ヘッダーを追加
+	req.Header.Add("X-SDTD-API-TOKENNAME", m.PlayersAPIUser)
+	req.Header.Add("X-SDTD-API-SECRET", m.PlayersAPISecret)
+	// HTTPクライアントを作成
+	client := &http.Client{}
+	// リクエストを送信
+	resp, err := client.Do(req)
+	if err != nil {
+		if m.Debug {
+			log.Printf("REQUEST:\n%s", reqDump(req))
+		}
+		log.Fatalf("Error sending request  err:%s", err)
+		return res
+	}
+	defer resp.Body.Close()
+
+	// レスポンスを読み込む
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		if m.Debug {
+			log.Printf("RESPONSE:\n%s", respDump(resp))
+		}
+		log.Fatalf("Error reading response body: ", err)
+		return res
+	}
+	// JSONをパース
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		if m.Debug {
+			log.Printf("RESPONSE:\n%s", respDump(resp))
+		}
+		log.Fatalf("Error parsing JSON:%s ", err)
+	}
+	return res
+}
+
+func trimSteam(steamID string) string {
+	return strings.TrimPrefix(steamID, "Steam_")
+}
+
+func (m *mackerelAPI) createMetrics(players []Player, now time.Time) []*mackerel.MetricValue {
+	res := make([]*mackerel.MetricValue, 0, len(players)*4)
+	for _, player := range players {
+		id := trimSteam(player.SteamID)
+		if player.Online {
+			res = append(res, &mackerel.MetricValue{
+				Name:  "custom.player.level." + id,
+				Time:  now.Unix(),
+				Value: player.Level,
+			})
+			res = append(res, &mackerel.MetricValue{
+				Name:  "custom.player.x." + id,
+				Time:  now.Unix(),
+				Value: player.Position.X,
+			})
+			res = append(res, &mackerel.MetricValue{
+				Name:  "custom.player.y." + id,
+				Time:  now.Unix(),
+				Value: player.Position.Y,
+			})
+			res = append(res, &mackerel.MetricValue{
+				Name:  "custom.player.totalplaytime." + id,
+				Time:  now.Unix(),
+				Value: float64(player.TotalPlayTime),
+			})
+		}
+	}
+	return res
+}
+
+func (m *mackerelAPI) postGraphDef(data []MetricDef) {
+	url := "https://api.mackerelio.com/api/v0/graph-defs/create"
+	m.post(url, data)
+}
+
+/*
+func (m *mackerelAPI) postMetrics(data []MetricValue) {
+	url := "https://api.mackerelio.com/api/v0/tsdb"
+	m.post(url, m.setHostID(data))
+}
+*/
+
+func (m *mackerelAPI) post(url string, data any) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("Error marshaling metrics: %v", err)
+	}
+	if m.Debug {
+		log.Printf("Posting metrics to url:%s: %s", url, jsonData)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Api-Key", m.MackerelAPIKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("REQUEST:\n%s", reqDump(req))
+		log.Fatalf("Error posting metrics to Mackerel: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("REQUEST:\n%s", reqDump(req))
+		log.Printf("RESPONSE:\n%s", respDump(resp))
+		log.Fatalf("Received non-200 response: %d", resp.StatusCode)
+	}
+
+	log.Println("Metrics posted successfully")
+}
+
+func getSteamIDs(players []Player) []string {
+	ids := make([]string, len(players))
+	for i, player := range players {
+		ids[i] = trimSteam(player.SteamID)
+	}
+	return ids
+}
+
+func compeareSteamIDs(steamIDs1, steamIDs2 []string) bool {
+	if len(steamIDs1) != len(steamIDs2) {
+		return false
+	}
+	for i := range steamIDs1 {
+		if steamIDs1[i] != steamIDs2[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func makeDef(players []Player) []MetricDef {
+	metricDefs := make([]MetricDef, 0, len(players)*4)
+	for _, player := range players {
+		id := trimSteam(player.SteamID)
+		metricDefs = append(metricDefs, MetricDef{
+			Name:        "custom.player.level",
+			DisplayName: "レベル",
+			Unit:        "integer",
+			// "float", "integer", "percentage", "seconds", "milliseconds",
+			//"bytes", "bytes/sec", "bits/sec", "iops"
+			Metrics: []MetricDetail{
+				{
+					Name:        "custom.player.level." + id,
+					DisplayName: player.Name,
+					IsStacked:   false,
+				},
+			},
+		})
+		metricDefs = append(metricDefs, MetricDef{
+			Name:        "custom.player.x",
+			DisplayName: "位置X",
+			Unit:        "float",
+			// "float", "integer", "percentage", "seconds", "milliseconds",
+			//"bytes", "bytes/sec", "bits/sec", "iops"
+			Metrics: []MetricDetail{
+				{
+					Name:        "custom.player.x." + id,
+					DisplayName: player.Name,
+					IsStacked:   false,
+				},
+			},
+		})
+		metricDefs = append(metricDefs, MetricDef{
+			Name:        "custom.player.y",
+			DisplayName: "位置Y",
+			Unit:        "float",
+			// "float", "integer", "percentage", "seconds", "milliseconds",
+			//"bytes", "bytes/sec", "bits/sec", "iops"
+			Metrics: []MetricDetail{
+				{
+					Name:        "custom.player.y." + id,
+					DisplayName: player.Name,
+					IsStacked:   false,
+				},
+			},
+		})
+		metricDefs = append(metricDefs, MetricDef{
+			Name:        "custom.player.totalplaytime",
+			DisplayName: "プレイ時間",
+			Unit:        "seconds",
+			// "float", "integer", "percentage", "seconds", "milliseconds",
+			//"bytes", "bytes/sec", "bits/sec", "iops"
+			Metrics: []MetricDetail{
+				{
+					Name:        "custom.player.totalplaytime." + id,
+					DisplayName: player.Name,
+					IsStacked:   false,
+				},
+			},
+		})
+	}
+	return metricDefs
+}
+
+func (m *mackerelAPI) job() {
+	players := m.getPlayersOnline()
+	ids := getSteamIDs(players)
+	if len(ids) == 0 {
+		if m.Debug {
+			log.Println("No players online")
+		}
+		return
+	}
+	if !compeareSteamIDs(m.steamIDs, ids) {
+		m.postGraphDef(makeDef(players))
+		m.steamIDs = ids
+		if err := saveState(m.stateFile, m.steamIDs); err != nil {
+			log.Println(err)
+		}
+	}
+	metrics := m.createMetrics(players, time.Now())
+	if m.Debug {
+		log.Println(jsonDump(metrics))
+		return
+	}
+	err := m.mkr.PostHostMetricValuesByHostID(m.MackerelHostID, metrics)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func readState(file string, v any) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewDecoder(f).Decode(v)
+}
+func saveState(file string, v any) error {
+	f, err := os.Create(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return json.NewEncoder(f).Encode(v)
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	e := env{}
+	if err := envconfig.Process("", &e); err != nil {
+		log.Fatal(err)
+	}
+	tmpDir := os.TempDir()
+	uid := os.Getuid()
+	dir := filepath.Join(tmpDir, fmt.Sprintf("%s_%d", stateDirName, uid))
+	fpath := filepath.Join(dir, stateFileName)
+	m := &mackerelAPI{e, mackerel.NewClient(e.MackerelAPIKey), []string{}, fpath}
+	os.MkdirAll(dir, 0755)
+	if err := readState(fpath, &m.steamIDs); err != nil {
+		m.steamIDs = []string{}
+		saveState(fpath, m.steamIDs)
+		log.Printf("Create State file: %s", fpath)
+	}
+	m.job()
+}
